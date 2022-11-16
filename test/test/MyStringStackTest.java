f7404355da49fce4d336164e222a496deb068bc7
Syahmi Zulkifli <syahmi.zulkifli@ncs.com.sg>
2022-11-16 12:20:50 +0800

    Commit fix
    Signed-off-by: Syahmi Zulkifli <syahmi.zulkifli@ncs.com.sg>

--- test/test/MyStringStackTest.java
+++ test/test/MyStringStackTest.java
@@ -34,7 +34,6 @@ public class MyStringStackTest extends TestCase {
 		String o1 = null;  
 		MyStringStack stack = new MyStringStack();
 		stack.push (s1);
-		assertFalse (stack.isEmpty());
 		stack.push (s1);
         stack.push (s2);
         assertFalse (stack.isEmpty());
